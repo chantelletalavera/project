ccb4019183a93934e10bed97fdd80367267e78d4
John Doe <johndoe@foobar.com>
2019-05-19 17:45:59 +0800

    Added PDO connection string

--- connection.php
+++ connection.php
@@ -6,6 +6,8 @@ $dbname = "db_lumed";
 
 $conn = new mysqli($dbservername,$dbusername,$dbpassword,$dbname);
 
+$pdo = new PDO('mysql:host=localhost;dbname=db_lumed;charset=utf8','root','');
+
 if($conn->connect_error){
 	die("Connection Failed:".$conn->connect_error);
 }
